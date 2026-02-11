0cf29ace19dce6a9900dae04a14b6829c7918030
Lucas Henrique <lucashenriquedesouza2@gmail.com>
2026-02-11 07:08:59 +0000

    Refactor portfolio structure, improve UI, and implement dark/light mode with localStorage

--- js/script.js
+++ js/script.js
@@ -47,6 +47,8 @@ navAbout.addEventListener("click", () => {
     });
   });
 });
+
+
 contactBtn.addEventListener("click", () => {
   // espera um frame pra garantir que ela esteja visível
   requestAnimationFrame(() => {
@@ -56,6 +58,32 @@ contactBtn.addEventListener("click", () => {
     });
   });
 });
+
+
+ const toggleBtn = document.getElementById("theme-toggle");
+ const body = document.body;
+
+
+
+ // carregar preferência salva
+ if (localStorage.getItem("theme") === "light") {
+		body.classList.add("light-mode");
+		toggleBtn.textContent = "Dark Mode";
+ }
+
+ toggleBtn.addEventListener("click", () => {
+		body.classList.toggle("light-mode");
+
+		if (body.classList.contains("light-mode")) {
+			toggleBtn.textContent = "Dark Mode";
+			localStorage.setItem("theme", "light");
+		} else {
+			toggleBtn.textContent = "Light Mode";
+			localStorage.setItem("theme", "dark");
+		}
+ });
+
+
 // Fechar seção About Me ao clicar no botão "X"
 closeAbout.addEventListener("click", () => {
   aboutSection.classList.add("hide");
