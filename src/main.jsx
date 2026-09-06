8ff837f82b4a7b69c50a7bad81e7b6f339f0458b
Lucas Henrique <lucashenriquedesouza2@gmail.com>
2026-09-06 13:37:37 +0100

    feat: create visitor context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { VisitorProvider } from './context/VisitorContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/myPortf-lio">
-      <App />
+      <VisitorProvider>
+        <App />
+      </VisitorProvider>
     </BrowserRouter>
   </StrictMode>,
 )
